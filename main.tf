bcc018b99bed8dc48667dcda7fbf552b565e1f40
hholland923 <59858223+hholland923@users.noreply.github.com>
2021-01-21 11:06:00 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "google_compute_instance" "default" {
+  project      = "test-kitchen-271420"
+  name         = "test"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
